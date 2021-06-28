81d1b5130d570b666aca88680420116f3d543450
Ana Carolina Castro <ana.castro@corp.globo.com>
2021-06-28 18:14:14 -0300

    Refactor

--- tennis.rb
+++ tennis.rb
@@ -1,6 +1,13 @@
 
 class TennisGame1
 
+  POINTS_LABELS = {
+              0 => "Love",
+              1 => "Fifteen",
+              2 => "Thirty",
+              3 => "Forty",
+            }
+
   def initialize(player1Name, player2Name)
     @player1Name = player1Name
     @player2Name = player2Name
@@ -9,7 +16,7 @@ class TennisGame1
   end
         
   def won_point(playerName)
-    if playerName == "player1"
+    if playerName == @player1Name
       @p1points += 1
     else
       @p2points += 1
@@ -17,44 +24,27 @@ class TennisGame1
   end
   
   def score
-    result = ""
-    tempScore=0
-    if (@p1points==@p2points)
-      result = {
-          0 => "Love-All",
-          1 => "Fifteen-All",
-          2 => "Thirty-All",
-      }.fetch(@p1points, "Deuce")
-    elsif (@p1points>=4 or @p2points>=4)
-      minusResult = @p1points-@p2points
-      if (minusResult==1)
-        result ="Advantage player1"
-      elsif (minusResult ==-1)
-        result ="Advantage player2"
-      elsif (minusResult>=2)
-        result = "Win for player1"
-      else
-        result ="Win for player2"
-      end
+    if (@p1points >= 3 && @p1points == @p2points)
+      result = "Deuce"
+    elsif (@p1points >= 4 || @p2points >= 4)
+      result = advantage
     else
-      (1...3).each do |i|
-        if (i==1)
-          tempScore = @p1points
-        else
-          result+="-"
-          tempScore = @p2points
-        end
-        result += {
-            0 => "Love",
-            1 => "Fifteen",
-            2 => "Thirty",
-            3 => "Forty",
-        }[tempScore]
-      end
+      result = "#{POINTS_LABELS[@p1points]}-"
+      result += @p1points == @p2points ? "All" : POINTS_LABELS[@p2points]
     end
     result
   end
 end
+
+def advantage
+  score_difference = @p1points-@p2points
+  adv_player = score_difference.positive? ? "player1" : "player2"
+  
+  return "Advantage #{adv_player}" if (score_difference.abs == 1)
+
+  "Win for #{adv_player}"
+end
+
 
 class TennisGame2
   def initialize(player1Name, player2Name)
